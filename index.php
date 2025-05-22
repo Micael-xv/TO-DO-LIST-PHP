2e87bdf0e2e8335d2f5460c8b18dd2fea650db5d
Micael <micaelxv1@gmail.com>
2025-05-22 13:58:05 -0300

    feat: finalizado

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 require_once 'database/conn.php';
+
+$tasks = [];
+
+$sql = $pdo->query("SELECT * FROM task ORDER BY id ASC");
+
+if ($sql->rowCount() > 0) {
+  $tasks = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +24,7 @@ require_once 'database/conn.php';
   <div id="to_do">
     <h1>Things to do</h1>
 
-    <form action="" class="to-do-form">
+    <form action="actions/create.php" method="POST" class="to-do-form">
       <input type="text" name="description" placeholder="White your task here" required>
       <button type="submit" class="form-button">
         <i class="fa-solid fa-plus"></i>
@@ -24,30 +32,42 @@ require_once 'database/conn.php';
     </form>
 
     <div id="task">
-      <div class="task">
-        <input type="checkbox" name="progress" class="progress">
+      <?php foreach ($tasks as $task) : ?>
+        <div class="task">
+          <input
+            type="checkbox"
+            name="progress"
+            class="progress <?= $task['completed'] ? 'done' : '' ?>"
+            data-task-id="<?= $task['id'] ?>"
+            <?= $task['completed'] ? 'checked' : '' ?>>
 
-        <p class="task-description">
-          Tema de casa de PHP
-        </p>
+          <p class="task-description">
+            <?= $task['description'] ?>
+          </p>
 
-        <div class="task-actions">
-          <a class="action-button edit-button">
-            <i class="fa-regular fa-pen-to-square"></i>
-          </a>
+          <div class="task-actions">
+            <a class="action-button edit-button">
+              <i class="fa-regular fa-pen-to-square"></i>
+            </a>
 
-          <a class="action-button delete-button">
-            <i class="fa-regular fa-trash-can"></i>
-          </a>
+            <a href="actions/delete.php?id=<?= $task['id'] ?>" class="action-button delete-button">
+              <i class="fa-regular fa-trash-can"></i>
+            </a>
+          </div>
+
+          <form action="actions/update.php" method="POST" class="to-do-form edit-task hidden">
+            <input type="hidden" name="id" value="<?= $task['id'] ?>">
+            <input
+              type="text"
+              name="description"
+              placeholder="Edit your task here"
+              value="<?= $task['description'] ?>">
+            <button type="submit" class="form-button confirm-button">
+              <i class="fa-solid fa-check"></i>
+            </button>
+          </form>
         </div>
-
-        <form action="" class="to-do-form edit-task hidden">
-          <input type="text" name="description" placeholder="Edit your task here">
-          <button type="submit" class="form-button confirm-button">
-            <i class="fa-solid fa-check"></i>
-          </button>
-        </form>
-      </div>
+      <?php endforeach; ?>
     </div>
   </div>
 
--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // edita a tarefa
   document.querySelectorAll(".edit-button").forEach(function (button) {
     button.addEventListener("click", function () {
-      const task = this.closest(".task");
+      var task = button.closest(".task");
       task.querySelector(".progress").classList.add("hidden");
       task.querySelector(".task-description").classList.add("hidden");
       task.querySelector(".task-actions").classList.add("hidden");
@@ -11,11 +12,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.querySelectorAll(".progress").forEach(function (checkbox) {
     checkbox.addEventListener("click", function () {
-      if (this.checked) {
-        this.classList.add("done");
+      if (checkbox.checked) {
+        checkbox.classList.add("done");
       } else {
-        this.classList.remove("done");
+        checkbox.classList.remove("done");
       }
+    });
+
+    checkbox.addEventListener("change", function () {
+      const id = checkbox.dataset.taskId;
+      const completed = checkbox.checked ? "true" : "false";
+
+      fetch("../../actions/update-progress.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `id=${encodeURIComponent(id)}&completed=${encodeURIComponent(
+          completed
+        )}`,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (!data.success) {
+            alert("Erro ao editar a tarefa");
+          }
+        })
+        .catch(() => {
+          alert("Ocorreu um erro");
+        });
     });
   });
 });
